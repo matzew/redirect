4368b5c2eb0f22d95e1723ded92d4d5750501ac9
Christos Vasilakis <cvasilak@gmail.com>
2013-08-21 15:51:58 +0300

    redirect initial fix

--- redirectTests/redirectTests.m
+++ redirectTests/redirectTests.m
@@ -29,6 +29,58 @@
     [super tearDown];
 }
 
+- (void)testHTTPModified
+{
+    
+    // http ....
+    _client = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"http://secure-pushee.rhcloud.com/ag-push"]];
+    _client.parameterEncoding = AFJSONParameterEncoding;
+    
+    // apply HTTP Basic:
+    [_client setAuthorizationHeaderWithUsername:@"608acb1c-96b7-47a7-9663-ac9eb8781e04" password:@"0ad0ee26-5f54-4224-9739-41d67e21c3b0"];
+    
+    NSMutableURLRequest *request = [_client requestWithMethod:@"POST" path:@"rest/registry/device" parameters:@{@"deviceToken": @"122343223"}];
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+
+        NSLog(@"Success!");
+        _finished = YES;
+        
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        NSLog(@"ERROR: %@", error);
+        _finished = YES;
+        
+        // this fails, due to a '405' response code;
+        // on the server (http access log) I see a GET is issued against:
+        // http://secure-pushee.rhcloud.com/ag-push/rest/registry/device
+        
+        // However we are submitting a POST..... A post with CURL against the 'http' returns the expected 302 code...
+        
+        STFail(@"%@", error);
+    }];
+    
+    [operation setRedirectResponseBlock:^NSURLRequest *(NSURLConnection *connection, NSURLRequest *req, NSURLResponse *redirectResponse) {
+
+        if (redirectResponse != nil) {
+            request.URL = req.URL;
+            
+            return request;
+        }
+
+        return request;
+    }];
+    
+    
+    [_client enqueueHTTPRequestOperation:operation];
+    
+    
+    while(!_finished) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+    }
+    
+}
+
+/*
 - (void)testHTTP
 {
     
@@ -123,5 +175,5 @@
     }
     
 }
-
+*/
 @end
