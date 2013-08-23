67ac5a0835a05ad5d8e534292465542d82562881
Christos Vasilakis <cvasilak@gmail.com>
2013-08-23 11:32:19 +0300

    fix redirection content-type

--- redirectTests/redirectTests.m
+++ redirectTests/redirectTests.m
@@ -29,92 +29,58 @@
     [super tearDown];
 }
 
-- (void)testHTTPModified
+
+- (void)testHTTP
 {
-    
     // http ....
     _client = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"http://secure-pushee.rhcloud.com/ag-push"]];
     _client.parameterEncoding = AFJSONParameterEncoding;
     
     // apply HTTP Basic:
-    [_client setAuthorizationHeaderWithUsername:@"608acb1c-96b7-47a7-9663-ac9eb8781e04" password:@"0ad0ee26-5f54-4224-9739-41d67e21c3b0"];
+    [_client setAuthorizationHeaderWithUsername:@"608acb1c-96b7-47a7-9663-ac9eb8781e04"
+                                       password:@"0ad0ee26-5f54-4224-9739-41d67e21c3b0"];
     
-    NSMutableURLRequest *request = [_client requestWithMethod:@"POST" path:@"rest/registry/device" parameters:@{@"deviceToken": @"122343223"}];
+    NSMutableURLRequest *request = [_client requestWithMethod:@"POST"
+                                                         path:@"rest/registry/device"
+                                                   parameters:@{@"deviceToken": @"122343223"}];
+    
+    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
     
     AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-
+        
         NSLog(@"Success!");
+        
         _finished = YES;
         
     } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
         NSLog(@"ERROR: %@", error);
+        
         _finished = YES;
-        
-        // this fails, due to a '405' response code;
-        // on the server (http access log) I see a GET is issued against:
-        // http://secure-pushee.rhcloud.com/ag-push/rest/registry/device
-        
-        // However we are submitting a POST..... A post with CURL against the 'http' returns the expected 302 code...
-        
-        STFail(@"%@", error);
     }];
     
     [operation setRedirectResponseBlock:^NSURLRequest *(NSURLConnection *connection, NSURLRequest *req, NSURLResponse *redirectResponse) {
-
-        if (redirectResponse != nil) {
+        
+        if (redirectResponse != nil) {  // we got HTTP-302 redirection
+            
+            // NOTE:
+            //      As per Apple doc the req is 'the proposed redirected request'. But we cannot return it as it is. The reason is user-agents (and in our case NSUrlconnection) 'erroneous' after a 302-redirection modify the request http method to GET if the client performs a POST (as we do here). What we do here is to use the original request with just updating the URL to the new redirected one.
+            
+            // update URL of the original request
+            // to the 'new' redirected one
             request.URL = req.URL;
             
+            // return the original request
             return request;
         }
-
+        
         return request;
     }];
     
-    
     [_client enqueueHTTPRequestOperation:operation];
     
-    
     while(!_finished) {
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
     }
-    
-}
-
-/*
-- (void)testHTTP
-{
-    
-    // http ....
-    _client = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"http://secure-pushee.rhcloud.com/ag-push"]];
-    _client.parameterEncoding = AFJSONParameterEncoding;
-    
-    // apply HTTP Basic:
-    [_client setAuthorizationHeaderWithUsername:@"608acb1c-96b7-47a7-9663-ac9eb8781e04" password:@"0ad0ee26-5f54-4224-9739-41d67e21c3b0"];
-    
-    
-    [_client postPath:@"rest/registry/device" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"Success!");
-        
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"ERROR: %@", error);
-        _finished = YES;
-        
-        
-        // this fails, due to a '405' response code;
-        // on the server (http access log) I see a GET is issued against:
-        // http://secure-pushee.rhcloud.com/ag-push/rest/registry/device
-        
-        // However we are submitting a POST..... A post with CURL against the 'http' returns the expected 302 code...
-        
-        STFail(@"%@", error);
-        
-    }];
-    
-    
-    while(!_finished) {
-        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
-    }
-    
 }
 
 - (void)testHTTPs
@@ -143,37 +109,4 @@
     
 }
 
-- (void) testZSometingHttps {
-    
-    NSURL *url = [NSURL URLWithString:@"http://secure-pushee.rhcloud.com/ag-push"];//rest/registry/device"];
-    AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:url];
-    
-    NSMutableURLRequest *request = [httpClient requestWithMethod:@"POST" path:@"rest/registry/device" parameters:@{@"deviceToken": @"122343223"}];
-    
-    
-    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request
-                                         success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-                                             NSLog(@"\n\nSUCCESS\n\n");
-                                             
-                                         } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
-
-                                             _finished = YES;
-
-                                             // this fails, due to a '405' response code;
-                                             // on the server (http access log) I see a GET is issued against:
-                                             // http://secure-pushee.rhcloud.com/ag-push/rest/registry/device
-
-                                             // However we are submitting a POST..... A post with CURL against the 'http' returns the expected 302 code...
-
-                                             STFail(@"%@", error);
-
-                                         }];
-    
-    [operation start];
-    while(!_finished) {
-        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
-    }
-    
-}
-*/
 @end
